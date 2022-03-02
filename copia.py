239b4e487f1f81a4a150b4059d7918615e074a00
F4 <79203606+RonaldLSB@users.noreply.github.com>
2022-03-01 21:57:56 -0300

    Update copia.py

--- copia.py
+++ copia.py
@@ -1,9 +1,7 @@
-# Use para dar "ctrl v" em td texto que vc der 'ctrl c' quantas vezes quiser!
-
-import pyautogui, time
-time.sleep(4)
-for i in range(50):
-    pyautogui.hotkey('ctrl', 'v')
-    pyautogui.press("enter")
-    time.sleep(3)
-
+import pyautogui, time,keyboard
+time.sleep(3)
+while true:
+    while keyboard.is_pressed('2') == False:
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press("enter")
+ 
